be196cff6aaa345838bc4c03131d64759de8b044
tanishk050504 <Tsharma0219@conestogac.on.ca>
2023-03-10 17:51:03 -0800

    Unit Testing of Length and width.

--- UnitTest_CSCN7120_A2/UnitTest_CSCN7120_A2.cpp
+++ UnitTest_CSCN7120_A2/UnitTest_CSCN7120_A2.cpp
@@ -3,7 +3,8 @@
 
 extern "C" int getPerimeter(int*, int*);
 extern "C" int getArea(int*, int*);
-
+extern "C" int setLength(int , int*);
+extern "C" int setWidth(int, int*);
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTestCSCN7120A2
@@ -21,7 +22,6 @@ namespace UnitTestCSCN7120A2
 			TestResult = getPerimeter(&length,&width);
 			Assert::AreEqual(ExpectedResult, TestResult);
 		}
-
 		TEST_METHOD(TestgetArea)
 		{
 			int length = 12;
@@ -31,6 +31,44 @@ namespace UnitTestCSCN7120A2
 			TestResult = getArea(&length, &width);
 			Assert::AreEqual(ExpectedResult, TestResult);
 		}
-	
+
+		TEST_METHOD(setLengthInRange)
+		{
+			int length;
+			setLength(57, &length);
+			Assert::AreEqual(57,length);
+		}
+
+		TEST_METHOD(setLengthOutofRange)
+		{
+			int length;
+			setLength(101, &length);
+			Assert::AreEqual(101, length);
+		}
+
+		TEST_METHOD(setLengthNegative)
+		{
+			int length;
+			setLength(-57, &length);
+			Assert::AreEqual(-57, length);
+		}
+		TEST_METHOD(setWidthInRange)
+		{
+			int width;
+			setWidth(97, &width);
+			Assert::AreEqual(97, width);
+		}
+		TEST_METHOD(setWidthOutofRange)
+		{
+			int width;
+			setWidth(103, &width);
+			Assert::AreEqual(103,width);
+		}
+		TEST_METHOD(setWidthNegative)
+		{
+			int width;
+			setWidth(-1, &width);
+			Assert::AreEqual(-1, width);
+		}
 	};
 }
